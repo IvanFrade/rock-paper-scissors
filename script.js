93036f41162654825726bb0c044f3f14c0dce105
IvanFrade <fradellin.ivan@gmail.com>
2025-06-02 15:37:44 +0200

    Add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("test");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice() {
+    let choice = getRandomInt(3);
+
+    if (choice === 0) return "Rock";
+    else if (choice === 1) return "Paper";
+    else return "Scissors";
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
