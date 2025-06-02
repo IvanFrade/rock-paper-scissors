b863954bbd3b56e58e23d5fe0d806131eae1270e
IvanFrade <fradellin.ivan@gmail.com>
2025-06-02 15:54:54 +0200

    Add basic game logic

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ function getHumanChoice() {
     return prompt("Your choice: ", "Rock").toLowerCase();
 }
 
-function playRound() {
-    console.log("Playing round...");
+function playRound(n) {
+    console.log(`Playing round ${n}...`);
     
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
@@ -23,20 +23,32 @@ function playRound() {
     console.log(`Human choice: ${humanChoice}`);
     console.log(`Computer choice: ${computerChoice}`);
 
-    if (humanChoice === computerChoice) 
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
         return;
+    }
     else if (humanChoice === "rock") {
-        computerChoice === "paper" ? computerScore++ : humanScore++;
+        computerChoice === "paper" ? computerScore++ : humanScore++;        
     } else if (humanChoice === "paper") {
         computerChoice === "scissors" ? computerScore++ : humanScore++;
     } else if (humanChoice === "scissors") {
         computerChoice === "rock" ? computerScore++ : humanScore++;
+    }
+
+    console.log(`Current score:
+        Human: ${humanScore}
+        Computer: ${computerScore}`);
+}
+
+function playGame() {
+    let maxRounds = 5;
+
+    for (i = 1; i <= maxRounds; i++) {
+        playRound(i);
     }
 }
 
 let computerScore = 0;
 let humanScore = 0;
 
-playRound()
-console.log(`Human score: ${humanScore}`);
-console.log(`Computer score: ${computerScore}`);
+playGame();
